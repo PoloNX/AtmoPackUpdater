52775951274bd59f8a0bb5ad487aac312961fe6a
Zoria <50277488+THZoria@users.noreply.github.com>
2022-07-03 10:37:30 +0200

    fix include link Updaters full and pack full

--- includes/download.h
+++ includes/download.h
@@ -1,8 +1,8 @@
 #ifndef _DOWNLOAD_H_
 #define _DOWNLOAD_H_
 
-#define APP_URL         "https://github.com/PoloNX/AtmoPackUpdater/releases/latest/download/AtmoPackUpdater.nro"
-#define CFW_URL         "https://github.com/THZoria/AtmoPack-Vanilla/releases/latest/download/AtmoPack-Vanilla_Latest.zip"
+#define APP_URL         "https://github.com/THZoria/AtmoPackUpdater-Full/releases/latest/download/AtmoPackUpdater.nro"
+#define CFW_URL         "https://github.com/Pikatsuto/AtmosphereFullFetcher-cli/releases/latest/download/AtmoPack-Full.zip"
 #define SIG_URL         "https://github.com/ITotalJustice/patches/releases/latest/download/SigPatches.zip"
 //#define FIR_URL         "https://github.com/THZoria/NX_Firmware/releases/latest/download/Firmware.zip"
 
