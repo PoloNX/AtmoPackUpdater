d5251a335fdca226a8c413af762af49d482356b5
PoloNX <57038157+PoloNX@users.noreply.github.com>
2022-08-07 14:43:55 +0000

    delete amssu-rcm

--- amssu-rcm/bdk/libs/lvgl/lv_themes/lv_themes.mk
+++
@@ -1,9 +0,0 @@
-CSRCS += lv_theme.c
-CSRCS += lv_theme_default.c
-CSRCS += lv_theme_templ.c
-CSRCS += lv_theme_material.c
-
-DEPPATH += --dep-path $(LVGL_DIR)/lvgl/lv_themes
-VPATH += :$(LVGL_DIR)/lvgl/lv_themes
-
-CFLAGS += "-I$(LVGL_DIR)/lvgl/lv_themes"
