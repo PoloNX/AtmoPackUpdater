516ee74e6980b6e320bfc62b4d8780e16b8b3f45
PoloNX <geyrecharlie@gmail.com>
2022-08-08 00:08:21 +0200

    update scripts

--- make.bat
+++
@@ -1,9 +0,0 @@
- cd amssu-rcm
- make
- cd ..
- cd amssu-forwarder
- make
- cd ..
- copy amssu-rcm/output/ams_rcm.bin resources/payload/ams_rcm.bin
- copy amssu-forwarder/amssu-forwarder.nro resources/forwarder/amssu-forwarder.nro
- make
